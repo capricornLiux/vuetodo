93a0be8e67dcea20a8447793022496817cd11ca8
liuxin <784532927@qq.com>
2018-03-21 22:46:11 +0800

    提取css文件

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const webpack = require('webpack');
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 // 判断是否是开发模式
 const isDev = process.env.NODE_ENV === 'development'
@@ -12,7 +13,7 @@ const config = {
     entry: path.join(__dirname, 'src/index.js'),
     
     output: {
-        filename: 'bundle.js',
+        filename: '[name].[hash:8].js',
         path: path.join(__dirname, 'dist')
     },
     module: {
@@ -24,25 +25,6 @@ const config = {
             {
                 test: /\.jsx$/,
                 loader: 'babel-loader'
-            },
-            {
-                test: /\.css$/,
-                use: [ 'style-loader', 'css-loader' ]
-            },
-            {
-                test: /\.styl$/,
-                use: [
-                    'style-loader',
-                    'css-loader',
-                    {
-                        loader: 'postcss-loader',
-                        options: {
-                            sourceMap: true
-                            // 使用stylus-loader生成的sourcemap
-                        }
-                    },
-                    'stylus-loader'
-                ]
             },
             {
                 test: /\.(gif|jpg|jpeg|png|svg)$/,
@@ -74,6 +56,24 @@ const config = {
 if (isDev) {
     // 是开发模式
 
+    // 添加打包样式文件
+    config.module.rules.push(
+        {
+            test: /\.styl$/,
+            use: [
+                'style-loader',
+                'css-loader',
+                {
+                    loader: 'postcss-loader',
+                    options: {
+                        sourceMap: true
+                        // 使用stylus-loader生成的sourcemap
+                    }
+                },
+                'stylus-loader'
+            ]
+        }
+    )
     // 开启调试模式
     config.devtool = '#cheap-module-eval-source-map'
 
@@ -96,6 +96,32 @@ if (isDev) {
     config.plugins.push(
         new webpack.HotModuleReplacementPlugin(),
         new webpack.NoEmitOnErrorsPlugin()
+    )
+} else {
+    // 生产环境
+    config.output.filename = '[name].[chunkhash:8].js'
+    config.module.rules.push(
+        {
+            test: /\.styl$/,
+            use: ExtractTextPlugin.extract({
+                fallback: 'style-loader',
+                use: [
+                    'css-loader',
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            sourceMap: true
+                            // 使用stylus-loader生成的sourcemap
+                        }
+                    },
+                    'stylus-loader'
+                ]
+            })
+        }
+    )
+
+    config.plugins.push(
+        new ExtractTextPlugin('style.[contentHash:8].css')
     )
 }
 

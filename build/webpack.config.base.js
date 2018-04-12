7e1437867dd940dd12a8541dfdcf671189338636
liuxin <784532927@qq.com>
2018-04-13 00:53:39 +0800

    配置demo环境

--- build/webpack.config.base.js
+++ build/webpack.config.base.js
@@ -1,59 +1,59 @@
 // 所有webpack配置的公共部分
-const path = require('path');
+const path = require('path')
 
 // 引入vue-loader配置的方法
-const createVueLoaderOptions = require('./vue-loader.config');
+const createVueLoaderOptions = require('./vue-loader.config')
 
 const isDev = process.env.NODE_ENV === 'development'
 
 const config = {
-    // webpack编译为类浏览器环境
-    target: 'web',
+  // webpack编译为类浏览器环境
+  target: 'web',
 
-    entry: path.join(__dirname, '../client/index.js'),
-     
-    output: {
-        filename: '[name].[hash:8].js',
-        // 开发环境不能使用chunkHash, 会报错
-        path: path.join(__dirname, '../dist')
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                loader: "eslint-loader",
-                enforce: "pre"
-                // 在其他loader处理之前处理
-            },
-            {
-                test: /\.vue$/,
-                loader: 'vue-loader',
-                options: createVueLoaderOptions(isDev)
-            },
-            {
-                test: /\.jsx$/,
-                loader: 'babel-loader'
-            },
-            {
-                test: /\.(gif|jpg|jpeg|png|svg)$/,
-                use: [
-                    {
-                        loader: 'url-loader',
-                        options: {
-                            limit: 1024,
-                            name: 'resources/[path][name].[hash:8].[ext]'
-                        }
-                    }
-                ]
-            },
-            {
-                test: /\.js$/,
-                loader: 'babel-loader',
-                exclude: /node_modules/
+  entry: path.join(__dirname, '../client/index.js'),
+
+  output: {
+    filename: '[name].[hash:8].js',
+    // 开发环境不能使用chunkHash, 会报错
+    path: path.join(__dirname, '../dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'eslint-loader',
+        enforce: 'pre'
+        // 在其他loader处理之前处理
+      },
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+        options: createVueLoaderOptions(isDev)
+      },
+      {
+        test: /\.jsx$/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.(gif|jpg|jpeg|png|svg)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 1024,
+              name: 'resources/[path][name].[hash:8].[ext]'
             }
+          }
         ]
-    }
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      }
+    ]
+  }
 }
 
-module.exports = config;
+module.exports = config
--- build/webpack.config.client.js
+++ build/webpack.config.client.js
@@ -1,144 +1,141 @@
-const merge = require('webpack-merge');
+const merge = require('webpack-merge')
 
-const path = require('path');
-const webpack = require('webpack');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const path = require('path')
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 // 导入基础配置
-const webpackBaseConfig = require('./webpack.config.base');
+const webpackBaseConfig = require('./webpack.config.base')
 
 // 判断是否是开发模式
 const isDev = process.env.NODE_ENV === 'development'
 
 // 最终导出的webpack配置
-let config;
+let config
 
 // 单独拿出 webpack dev server的配置, 方便管理
 const devServer = {
-    port: 9000,
-    host: '0.0.0.0',
-    open: true,
-    overlay: {
-        errors: true
-    },
-    hot: true
+  port: 9000,
+  host: '0.0.0.0',
+  open: true,
+  overlay: {
+    errors: true
+  },
+  hot: true
 }
 
 // 默认的plugins
 const defaultPlugins = [
-    // 配置前端代码中的环境变量
-    new webpack.DefinePlugin({
-        'process.env':{
-            NODE_ENV: isDev ? '"development"' : '"production"'
-        }
-    }),
+  // 配置前端代码中的环境变量
+  new webpack.DefinePlugin({
+    'process.env': {
+      NODE_ENV: isDev ? '"development"' : '"production"'
+    }
+  }),
 
-    // 自动生成html
-    new HtmlWebpackPlugin()
+  // 自动生成html
+  new HtmlWebpackPlugin()
 ]
 
 // 判断是否是开发模式
 if (isDev) {
-    // 是开发模式
+  // 是开发模式
 
-    // 进行合并 (合并基础配置和开发配置)
-    config = merge(webpackBaseConfig, {
+  // 进行合并 (合并基础配置和开发配置)
+  config = merge(webpackBaseConfig, {
 
-        // 配置loader
-        module : {
-            rules: [
-                // 处理样式文件
-                {
-                    test: /\.styl$/,
-                    use: [
-                        'vue-style-loader',
-                        // 
-                        {
-                            loader: 'css-loader',
-                            options: {
-                                modules: true,
-                                localIdentName: '[hash:base64:5]',
-                                camelCase: true
-                            }
-                        },
-                        {
-                            loader: 'postcss-loader',
-                            options: {
-                                sourceMap: true
-                                // 使用stylus-loader生成的sourcemap
-                            }
-                        },
-                        'stylus-loader'
-                    ]
-                }
-            ]
-        },
+    // 配置loader
+    module: {
+      rules: [
+        // 处理样式文件
+        {
+          test: /\.styl$/,
+          use: [
+            'vue-style-loader',
+            {
+              loader: 'css-loader',
+              options: {
+                modules: true,
+                localIdentName: '[hash:base64:5]',
+                camelCase: true
+              }
+            },
+            {
+              loader: 'postcss-loader',
+              options: {
+                sourceMap: true
+                // 使用stylus-loader生成的sourcemap
+              }
+            },
+            'stylus-loader'
+          ]
+        }
+      ]
+    },
 
-        // 开启调试模式
-        devtool: '#cheap-module-eval-source-map',
+    // 开启调试模式
+    devtool: '#cheap-module-eval-source-map',
 
-        // 配置dev server
-        devServer,
+    // 配置dev server
+    devServer,
 
-        // 配置plugins
-        plugins: defaultPlugins.concat([
-            new webpack.HotModuleReplacementPlugin(),
-            new webpack.NoEmitOnErrorsPlugin()
-        ])
-    })
-
+    // 配置plugins
+    plugins: defaultPlugins.concat([
+      new webpack.HotModuleReplacementPlugin(),
+      new webpack.NoEmitOnErrorsPlugin()
+    ])
+  })
 } else {
-
-    // 生产环境
-    config = merge(webpackBaseConfig, { 
-        entry: {
-            app: path.join(__dirname, '../client/index.js'),
-            // 单独打包
-            vendor: ['vue']
-        }, 
-        output: {
-            filename: '[name].[chunkhash:8].js'
-        },
-        module: {
-            rules: [
-                {
-                    test: /\.styl$/,
-                    use: ExtractTextPlugin.extract({
-                        fallback: 'vue-style-loader',
-                        use: [
-                            'css-loader',
-                            {
-                                loader: 'postcss-loader',
-                                options: {
-                                    sourceMap: true
-                                    // 使用stylus-loader生成的sourcemap
-                                }
-                            },
-                            'stylus-loader'
-                        ]
-                    })
+  // 生产环境
+  config = merge(webpackBaseConfig, {
+    entry: {
+      app: path.join(__dirname, '../client/index.js'),
+      // 单独打包
+      vendor: ['vue']
+    },
+    output: {
+      filename: '[name].[chunkhash:8].js'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.styl$/,
+          use: ExtractTextPlugin.extract({
+            fallback: 'vue-style-loader',
+            use: [
+              'css-loader',
+              {
+                loader: 'postcss-loader',
+                options: {
+                  sourceMap: true
+                  // 使用stylus-loader生成的sourcemap
                 }
+              },
+              'stylus-loader'
             ]
-        },
+          })
+        }
+      ]
+    },
 
-        plugins: defaultPlugins.concat([
-            // 提取css
-            new ExtractTextPlugin('style.[contentHash:8].css'),
+    plugins: defaultPlugins.concat([
+      // 提取css
+      new ExtractTextPlugin('style.[contentHash:8].css'),
 
-            // 提取第三方库
-            new webpack.optimize.CommonsChunkPlugin({
-                name: 'vendor'
-            }),
+      // 提取第三方库
+      new webpack.optimize.CommonsChunkPlugin({
+        name: 'vendor'
+      }),
 
-            // 中间插入的模块, 分配编号, 导致后面的模块编号发生变化, 导致长缓存失效, 解决这个问题
-            // 顺序在vendor后面
-            new webpack.optimize.CommonsChunkPlugin({
-                name: 'runtime',
-                minChunks: Infinity
-            })
-        ])
-    })
+      // 中间插入的模块, 分配编号, 导致后面的模块编号发生变化, 导致长缓存失效, 解决这个问题
+      // 顺序在vendor后面
+      new webpack.optimize.CommonsChunkPlugin({
+        name: 'runtime',
+        minChunks: Infinity
+      })
+    ])
+  })
 }
 
-module.exports = config;
+module.exports = config
